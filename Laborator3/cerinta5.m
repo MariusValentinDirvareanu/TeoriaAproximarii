e628780bc17e03e2004f1738cf9bcc01e79590c7
Marius Dirvareanu <dirvareanumarius@live.com>
2020-03-21 21:09:23 +0200

    Exercitiul 5

--- Laborator3/cerinta5.m
+++ Laborator3/cerinta5.m
@@ -6,6 +6,7 @@ yval=spline(x,y,xval);
 plot(x,y,'*');
 hold on;
 plot(xval,yval,'r');
+hold on;
 n=length(x);
 for i=1:n-1
     h(i)=x(i+1)-x(i);
@@ -13,7 +14,9 @@ end
 a=natsplinecoef(x,y);
 
 k1=a(2)/2*h(2)-y(1)/h(1)+h(1)*a(1)/6+y(2)/h(1)-h(1)*a(2)/6;
-%kn=a(n+1)/2*h(n+1)-y(n)/h(n)+h(n)*a(n)/6+y(n+1)/h(n)-h(n)*a(n+1)/6;
+kn=a(4)/2*h(4)-y(4-1)/h(4-1)+h(4-1)*a(4-1)/6+y(4)/h(4-1)-h(4-1)*a(4)/6;
 
 
-%spline(x, [k1,y,kn],xval);
+yval2=spline(x, [k1,y,kn],xval);
+plot(xval,yval2,'b');
+norm(abs(yval-yval2),Inf)
