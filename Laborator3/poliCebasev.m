244809e5f63fab2775b1eb86e05bce24d563bef4
Marius Dirvareanu <dirvareanumarius@live.com>
2020-03-22 16:28:52 +0200

    Raspunsuri

--- Laborator3/poliCebasev.m
+++ Laborator3/poliCebasev.m
@@ -9,7 +9,7 @@ n=11;
 for i=3:n+1
     T(i)=simplifyFraction((2*t*T(i-1)-T(i-2)),'Expand',true);
 end
-z=(double(solve(T(12)==0,t)))';
+z=(double(solve(T(8)==0,t)))';
 i=1;
 for j=1:length(z)
     if double(z(j))>=-a && double(z(j))<=a
@@ -31,13 +31,14 @@ disp(y);
 % Exercitiul 4
 m=length(x)-1;
 C=cmmp(x,y,m);
+disp('Valorile polinomului Lagrange de interpolare sunt:');
 disp(C);
 % Exercitiul 5
 figure(1);
 plot(x,y,'r');
 hold on;
-% plot(x,C(1)*x.^6+C(2)*x.^5+C(3)*x.^4+C(4)*x.^3+C(5)*x.^2+C(6)*x.^1+C(7),'b');
+plot(x,C(1)*x.^6+C(2)*x.^5+C(3)*x.^4+C(4)*x.^3+C(5)*x.^2+C(6)*x.^1+C(7),'b');
 % Exercitiul 6
-plot(x,C(1)*x.^10+C(2)*x.^9+C(3)*x.^8+C(4)*x.^7+C(5)*x.^6+C(6)*x.^5+C(7)*x.^4+C(8)*x.^3+C(9)*x.^2+C(10)*x.^1+C(11),'b');
+%plot(x,C(1)*x.^10+C(2)*x.^9+C(3)*x.^8+C(4)*x.^7+C(5)*x.^6+C(6)*x.^5+C(7)*x.^4+C(8)*x.^3+C(9)*x.^2+C(10)*x.^1+C(11),'b');
 axis([-a,a,min(y)-0.1,max(y)+0.1]);
 
