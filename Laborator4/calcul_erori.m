34f3ec93bc6769fcb8d065c46863cc2602565868
Marius Dirvareanu <dirvareanumarius@live.com>
2020-04-10 15:13:16 +0300

    Lab 4 calcul erori

--- Laborator4/calcul_erori.m
+++ Laborator4/calcul_erori.m
@@ -6,25 +6,25 @@ function [eroaref,eroarefprim]=calcul_erori(n)
     % aceasta este diviziunea
     x=linspace(0,pi/2,n+1);
     %inlocuiti A corespunzator
-    y=sin(x)+A;
+    y=sin(x)+10.2503;
     eroaref=0;
     eroarefprim=0;
     for i=1:n
         % calculam integralele din formula 2
         % c1,c2 sunt coef interpolantului asa ca el sa fie c1*x+c2
         % ei pot fi calculati direct din formula 1, sau pot fi calculati cu polyfit
-        c=polyfit(??????,1);
+        c=polyfit(x,y,1);
         c1=c(1);
         c2=c(2);
         %vrem sa calculam integrala functiei (sin(t)+A-(c1*t+c2))^2 pe intervalul [ x(i),x(i+1)],
         %vezi exemplul 0.1 de mai sus.
         %inlocuiti corespunzator
-        g=inline( '(sin(t)+?????????).^2','t','c1','c2');
-        er= quad(@(t)g(t,c1,c2),????????);
+        g=inline( '(sin(t)+10.2503-(c1*t+c2)).^2','t','c1','c2');
+        er= quad(@(t)g(t,c1,c2),0,pi/2);
         eroaref=eroaref+ er;
         %acum vrem sa calculam eroarea in aproximarea derivatei vezi a 2-a integrala in formula (2).
-        g=inline( '(cos(t)-??????).^2','t','c1');
-        er= quad(@(t)g(t,c1),???????);
+        g=inline( '(cos(t)-c1).^2','t','c1');
+        er= quad(@(t)g(t,c1),pi/2);
         eroarefprim=eroarefprim+ er;
     end
     eroaref=eroaref^0.5;
