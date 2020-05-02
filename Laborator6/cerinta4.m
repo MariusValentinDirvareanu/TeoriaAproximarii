5e8068271f125c52b791806a56b0e831fa3eb0b1
MariusValentinDirvareanu <dirvareanumarius@live.com>
2020-05-02 14:15:47 +0300

    Lab 6 incomplet

---
+++ Laborator6/cerinta4.m
@@ -0,0 +1,5 @@
+clc; clear all; close all;
+[er1,erprim1]=erori(4)
+[er2,erprim2]=erori(8)
+[er3,erprim3]=erori(16)
+[er4,erprim4]=erori(32)
--- Laborator6/cerinta5.m
+++ Laborator6/cerinta5.m
@@ -1,8 +1,8 @@
 clc; clear all; close all;
 x=linspace(0,1,10);%9 subintervale
 Ast=1.1;
-y=Ast*sin(pi*x);
+y=-6*x;
 c=fem1(9);
 norm(c'-y)
 xx=0:0.01:1;
-plot(xx,Ast*sin(pi*xx),'b',x,c,'r');
+plot(xx,-6*xx,'b',x,c,'r');
